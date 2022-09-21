4c70b3779c0dd1fcef9784021a3607070d1f993e
Md. Asif Rahman <asif@unlocklive.com>
2022-09-21 23:26:01 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <RouterLink class="navbar-brand" to="/">Blog</RouterLink>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/about">About</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    watch: {
+      $route: {
+        immediate: true,
+        handler(to, from) {
+          document.title = to.meta.title || 'Blog page';
+        }
+      },
+    }
+  };
+</script>
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      meta: { title: 'Home page' },
+      component: () => import('../views/home.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      meta: { title: 'About page' },
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
