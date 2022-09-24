b4ae5f53ddba65798aca8b917799ac8dab1f5d7c
Asif Rahman <asif@unlocklive.com>
2022-09-25 02:35:48 +0600

    Fontawesome added

--- src/component/Home.vue
+++ src/component/Home.vue
@@ -1,42 +1,44 @@
 <template>
-    <div v-for="(post, index) of posts" :key="index">
-        <h4>{{ post.title }}</h4>
-        <p>{{ post.body }}</p>
+    <div class="accordion accordion-flush my-5" id="blog_accordion">
+        <div class="accordion-item" v-for="(post, index) of posts" :key="index">
+            <h2 class="accordion-header" :id="`flush_heading_${index}`">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="`#flush_collapse_${index}`" aria-expanded="false" :aria-controls="`flush_collapse_${index}`">
+                    <span>{{ post.title }}</span>
+                    <small class="text-muted" style="position: absolute; right: 60px;">{{ post.user.name }} <font-awesome-icon icon="fa-regular fa-clock" /> {{ post.created_at }} </small>
+                </button>
+            </h2>
+            <div :id="`flush_collapse_${index}`" class="accordion-collapse collapse" :aria-labelledby="`flush_heading_${index}`" data-bs-parent="#blog_accordion">
+                <p>{{ post.body }}</p>
+
+                <h5>Comments</h5>
+                <div class="ms-3"  v-for="(comment, index) of post.comments" :key="index">
+                    <small class="text-muted">{{ comment.user.name }} <font-awesome-icon icon="fa-regular fa-clock" /> {{ comment.created_at }} </small>
+                    <p>{{ comment.comment }}</p>
+                </div>
+                <div class="ms-3">
+                    <small class="text-muted">Username</small>
+
+                    <div class="mb-3">
+                        <label for="write_comment" class="form-label text-secondary">Write a new comment</label>
+                        <textarea class="form-control" id="write_comment" rows="3"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import { useFetch } from '../composables/fetch.js';
-import { ref, computed } from 'vue';
 
 export default {
-    setup() {
-        let fetch_method = ref('get')
-        let fetch_url = ref('http://localhost:8000/api/v1/posts');
-
-        let { posts, errors } = useFetch(fetch_method, fetch_url);
-
-        setTimeout(function() {
-            console.log('posts :>> ', posts);
-            console.log('errors :>> ', errors);
-        }, 2000);
-
-        return { posts, errors };
-    },
     data: () => ({
-        // posts: {}
+        posts: {}
     }),
     created() {
-        var _this = this;
-        setTimeout(function() {
-            console.log(_this.posts);
-        }, 2000);
+        this.init();
     },
     mounted() {
-        var _this = this;
-        setTimeout(function() {
-            console.log(_this.posts);
-        }, 2000);
+        // 
     },
     updated() {
         // 
@@ -44,10 +46,9 @@ export default {
     methods: {
         init: function(){
             var _this = this;
-            console.log('Hello');
             this.$axios.get('http://localhost:8000/api/v1/posts')
             .then(function(res) {
-                console.log('res', res);
+                console.log('res :>> ', res);
 
                 _this.posts = Object.assign({}, res.data.data);
                 console.log('_this.posts', _this.posts);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from "./App.vue";
-import router from "./router";
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+/* import specific icons */
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+/* add icons to the library */
+library.add(faClock, faTimes);
 
 import "./assets/main.css";
 
@@ -15,6 +25,8 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.config.globalProperties.$axios = axios;
 
 app.mount("#app");
