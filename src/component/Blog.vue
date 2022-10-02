e4b11ac9777902a0f2e9f33ed1858b870c8e675d
Md. Asif Rahman <asif@unlocklive.com>
2022-10-03 02:33:13 +0600

    Write comment done

--- src/component/Blog.vue
+++ src/component/Blog.vue
@@ -4,7 +4,7 @@
             <h2 class="accordion-header" :id="`flush_heading_${index}`">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="`#flush_collapse_${index}`" aria-expanded="false" :aria-controls="`flush_collapse_${index}`">
                     <span>{{ post.title }}</span>
-                    <small class="text-muted" style="position: absolute; right: 60px;">{{ post.user.name }} <font-awesome-icon icon="fa-regular fa-clock" /> {{ post.created_at }} </small>
+                    <small class="text-muted" style="position: absolute; right: 60px;">{{ post?.user?.name }} <font-awesome-icon icon="fa-regular fa-clock" /> {{ post.created_at }} </small>
                 </button>
             </h2>
             <div :id="`flush_collapse_${index}`" class="accordion-collapse collapse" :aria-labelledby="`flush_heading_${index}`" data-bs-parent="#blog_accordion">
@@ -12,7 +12,7 @@
 
                 <h5>Comments</h5>
                 <div class="ms-3"  v-for="(comment, idx) of post.comments" :key="idx" ref="comment">
-                    <small class="text-muted">{{ comment.user.name }} <font-awesome-icon icon="fa-regular fa-clock" /> {{ comment.created_at }} </small>
+                    <small class="text-muted">{{ comment?.user?.name }} <font-awesome-icon icon="fa-regular fa-clock" /> {{ comment.created_at }} </small>
                     <p>{{ comment.comment }}</p>
                 </div>
                 <div v-if="authToken && Object.keys(authUser).length" class="row p-0 ms-1">
@@ -21,9 +21,9 @@
                     <div class="mb-3">
                         <label :for="`write_comment_${index}`" class="form-label text-secondary"><small>Write a new comment</small></label>
                         <textarea class="form-control" :id="`write_comment_${index}`" rows="2" ref="write_comment"></textarea>
-                    <div class="col-12 text-center">
 
-                        <button type="button" class="btn btn-primary float-end mt-2" @click="saveComment">Save</button>
+                        <div class="col-12 text-center">
+                            <button type="button" class="btn btn-primary float-end mt-2" @click="saveComment(index, post?.slug)">Save</button>
                         </div>
 
                     </div>
@@ -108,7 +108,34 @@
                     this.posts = result?.data || {};
                 }
             }
-        }
+        },
+        async saveComment(index, postSlug){
+                this.$emitter.emit('loadingStatus', true);
+
+                console.log('Update comment');
+
+                let req_url = `comment`;
+                let req_config = { method: 'POST', data: {
+                    post_slug: postSlug,
+                    comment: this.$refs.write_comment[index].value,
+                }};
+
+                const { result, errors, is_finished } = await this.excecuteAxios(req_url, req_config);
+                this.reqErrorMessage = errors?.message;
+
+                if(is_finished){
+                    this.$emitter.emit('loadingStatus', false);
+
+                    console.log('Post Comment Create response :>> ', result);
+
+                    if(!errors){
+                        let comment = result?.data;
+                        comment.user = this.authUser;
+                        this.posts[index].comments.push(comment);
+                        this.$refs.write_comment[index].value = '';
+                    }
+                }
+            },
     },
     computed: {
         // 
